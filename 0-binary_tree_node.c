7d10ca68476cfa7689eb536b44a3496e5945ada6
Benon <benonkighoma@gmail.com>
2023-02-27 13:22:46 +0300

    create binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,11 +13,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     {
         return NULL;
     }
-    if (!parent)
-    {
-        parent = new_node->parent;
-    }
     new_node->right= NULL;
     new_node->left = NULL;
+    new_node->parent = parent;
     new_node->n = value;
+    return (new_node);
 }
