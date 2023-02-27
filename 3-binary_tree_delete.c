2f0145ac5cd6b5690756d82b23c532f681ec6fca
Benon <benonkighoma@gmail.com>
2023-02-27 15:48:39 +0300

    delete tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete: delete entire binary tree
+ * @tree: root node of tree
+ * Return: Nothing*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (!tree)
+    {
+        return;
+    }
+    free(tree);
+}
