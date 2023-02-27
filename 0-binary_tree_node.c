210bd5fcb8f364ad6bc503d07080ce262838dbff
Benon <benonkighoma@gmail.com>
2023-02-27 12:57:55 +0300

    initial commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - create binary tree node
+ * @parent- root node
+ * @value - value of new_node
+ * Return: new node
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    if (new_node == NULL)
+    {
+        return NULL;
+    }
+    if (!parent)
+    {
+        parent = new_node->parent;
+    }
+    new_node->right= NULL;
+    new_node->left = NULL;
+    new_node->n = value;
+}
