1aac93d4f3849fff6738d6859c8d27f71e489d40
Benon <benonkighoma@gmail.com>
2023-02-28 11:44:14 +0300

    binary tree depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measure the depth of a binary tree
+ * @tree: pointer to the root node
+ * Return: depth
+ */
+size_t binary_tree_depth(binary_tree_t *tree)
+{
+	if (!tree || !tree->parent)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+}
