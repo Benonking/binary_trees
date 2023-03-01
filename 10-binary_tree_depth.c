a3312a78c443aed79af233b2cffb68d4fce43a43
Benon <benonkighoma@gmail.com>
2023-03-01 12:30:25 +0300

    return tree depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,7 +4,7 @@
  * @tree: pointer to the root node
  * Return: depth
  */
-size_t binary_tree_depth(binary_tree_t *tree)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree || !tree->parent)
 		return (0);
