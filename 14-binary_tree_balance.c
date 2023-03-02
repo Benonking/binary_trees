8f6a411df74b5a4ef923d9ca857fa33a8686357b
Benon <benonkighoma@gmail.com>
2023-03-02 17:20:05 +0300

    adjust balance file

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,28 +10,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		right_nodes = binary_tree_height(tree->right);
-		left_nodes = binary_tree_height(tree->left);
+		if (tree->right)
+		{
+			right_nodes = binary_tree_height(tree->right) + 1;
+		}
+		if (tree->left)
+		{
+			left_nodes = binary_tree_height(tree->left) + 1;
+		}
 		balance_factor = (left_nodes - right_nodes);
 		return (balance_factor);
-	}
-	return (0);
-}
-/**
- * binary_tree_height - calculate the height of a binary tree
- * @tree: pointer to the root node
- * Return: height
- */
-
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree)
-	{
-		size_t l = 0, r = 0;
-
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? l : r);
 	}
 	return (0);
 }
--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,14 +6,16 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		if ((tree->left != NULL && tree->right == NULL) ||
-			(tree->left == NULL && tree->right != NULL) ||
-			binary_tree_is_full(tree->left) == 0 ||
-			binary_tree_is_full(tree->right) == 0)
-
-			return (0);
-	}
-	return (1);
+		if (!tree)
+		{
+			return (1);
+		}
+		if (!tree->right && !tree->left)
+		{
+			return (1);
+		}
+		if (tree->right && tree->left) {
+			return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+		}
+		return (0);
 }
