b1d7c24b1c8a4e7486ecca9a21b9d72033edb062
Benon <benonkighoma@gmail.com>
2023-03-02 18:18:25 +0300

    siblings

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary _tree_is_perfect- check ifa binary tree is perfect
+ * @tree: pointer to the root node
+ * Return: 1 if true otherwise 0
+ */
+int binary _tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	if (tree->right && tree->left)
+	{
+	}
+}
